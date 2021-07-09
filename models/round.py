0edc23fce1a280f68b669b96afb7a558d8ff5faf
Selim Helaoui <helaoui.selim@gmail.com>
2021-07-09 11:44:39 +0200

    Création du fichier round.py contenant la class Round

---
+++ models/round.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel, validator
+
+from .match import Match
+
+class Round(BaseModel):
+    """Model representing a round of the chess tournament."""
+    name: str
+    start_time: datetime = datetime.now().isoformat()
+    end_time: datetime
+    matches : list
+
+    @validator("name")
+    def _check_len_name(cls, value: str):
+        if isinstance(value, str):
+            if len(value) > 25:
+                raise ValueError("Le nom ne doit pas dépasser 25 caractères.")
+        else:
+            raise ValueError("Le nom doit être une chaîne de caractères.")
+        return value
+    
+    @validator("matches")
+    def _check_matches(cls, value: List[dict]):
+        for match_data in value:
+            Match(**match_data)
+        #value = [Match(**match_data) for match_data in value]
+        return value
+
+
