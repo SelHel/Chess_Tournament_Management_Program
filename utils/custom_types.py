70df954b3beebe3e273d5f5c42cc06a6c337092a
Selim Helaoui <helaoui.selim@gmail.com>
2021-07-24 16:59:03 +0200

    création du dossier utils contenant le fichier manager et le fichier custom_types

---
+++ utils/custom_types.py
@@ -0,0 +1,14 @@
+import re
+from enum import Enum
+
+
+class Name(str):
+    def __init__(self, value: str):
+        if not re.match("^[a-zA-Z- 'éèïû]{2,25}$", value):
+            raise ValueError("Le nom n'est pas valide.")
+        super().__init__()
+
+
+class Gender(str, Enum):
+    Male = "Homme"
+    Female = "Femme"
