065b0e14d6b214d69e12a9ae060ba9cd413b04a0
Selim Helaoui <helaoui.selim@gmail.com>
2021-07-31 10:55:20 +0200

    ajout du formulaire, du menu, de la view et du fichier de config des paramètres de flake8

---
+++ utils/view.py
@@ -0,0 +1,18 @@
+import os
+
+
+class View:
+    def __init__(self, title: str, content: str = "", blocking: bool = False):
+        self.title = title
+        self.content = content
+        self.blocking = blocking
+
+    def display(self):
+        os.system("clear")
+        print(self.title)
+        print("-" * len(self.title))
+        if self.content:
+            if not self.blocking:
+                print(self.content)
+            else:
+                return input(self.content + "\n: ")
