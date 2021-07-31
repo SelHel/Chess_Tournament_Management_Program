658f185dd17782e45d5e21b591e3eb55e9785150
Selim Helaoui <helaoui.selim@gmail.com>
2021-07-31 10:51:29 +0200

    Mise à jour du manager

--- utils/manager.py
+++ utils/manager.py
@@ -8,20 +8,19 @@ class Manager:
         self.items = {}
         self.item_type = item_type
         self.id_finder = id_finder
-    
+
     def load_from_json(self, path: str):
         with open(path) as file:
             for item_data in json.load(file):
                 self.create_item(**item_data)
-    
+
     def create_item(self, *args, **kwargs):
         item = self.item_type(*args, **kwargs)
         self.items[self.id_finder(item)] = item
         return item
-    
+
     def find_all(self):
         return list(self.items.values())
-    
+
     def find_by_id(self, id: any) -> any:
         return self.items[id]
-
