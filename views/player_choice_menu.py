1efb063863a4533b37b8585431749f6d842737ec
Selim Helaoui <helaoui.selim@gmail.com>
2021-10-01 10:23:36 +0200

    ajout du player choice menu

---
+++ views/player_choice_menu.py
@@ -0,0 +1,10 @@
+from typing import List
+from models.player import Player
+from utils.menu import Menu
+
+
+class PlayerChoiceMenu(Menu):
+    def __init__(self, players: List[Player]):
+        super().__init__(
+            "Sélection des joueurs du tournoi",
+            [(f"{player.first_name} {player.last_name}", player.id) for player in players])
