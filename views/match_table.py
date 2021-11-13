83d0a7227e0fdc3aa1b607e4d27a083ae8a60f31
Selim Helaoui <helaoui.selim@gmail.com>
2021-11-13 09:25:44 +0100

    Amélioration de la match table.

--- views/match_table.py
+++ views/match_table.py
@@ -18,6 +18,6 @@ class MatchTable(Table):
         super().__init__("Liste des matchs du tournoi", matches, [
             ('Joueur 1', 20, lambda x: str(pm.find_by_id(x.id_player1))),
             ('Joueur 2', 40, lambda x: str(pm.find_by_id(x.id_player2))),
-            ('Score Joueur 1', 20, lambda x: x.score_player1),
-            ('Score Joueur 2', 30, lambda x: x.score_player2)
+            ('Score Joueur 1', 20, lambda x: x.score_player1.value),
+            ('Score Joueur 2', 30, lambda x: x.score_player2.value)
             ], [("Retour", "/tournaments")])
